3d2834dafea8aa58730b01d0dbc8f0576a330bd3
Ismael Sampaio <ismaelbrito2013@gmail.com>
2021-11-19 17:32:28 -0300

    questao 5

---
+++ 5.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+greater_file=""
+size=0
+while [ -n "${1}" ]; do
+    if [ -e "${1}" ]; then 
+        current_file_size=$(wc -l < ${1} )
+        if [[ ${current_file_size} -gt ${size} ]] ; then  
+            greater_file="${1}" 
+            size="${current_file_size}"
+        fi
+    else  
+        echo "erro: o arquivo ‘${1}’ não existe"
+    fi
+    
+shift
+done
+
+echo "Maior arquivo é : ${greater_file}"
